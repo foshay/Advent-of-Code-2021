c11e4af3dee70a8cd6975117aeac8af6e6e95ab9
Isak Foshay <25090017+foshay@users.noreply.github.com>
2021-12-04 14:11:43 -0800

    day 2 done. Half of day 3

---
+++ day 3/sol1.py
@@ -0,0 +1,18 @@
+val = [0] * 12
+rows = 0
+g = 0
+e = 0
+with open("input.txt","r") as fp:
+    lines = fp.readlines()
+    for x in lines:
+        for i in range(len(x)-1):
+            val[i]+=int(x[i])
+        rows+=1
+valLen = len(val)
+for i in range(valLen):
+    if val[i] > 500:
+        g+=pow(2,valLen-1-i)
+    else:
+        e+=pow(2,valLen-1-i)
+print(g*e)
+        
